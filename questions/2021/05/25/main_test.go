12cc6935174ff419fad2e4e8ef769525a76b9e0d
MrHuxu <hxtheone@gmail.com>
2021-05-25 16:47:53 +0800

    add the solution of 20210525

---
+++ questions/2021/05/25/main_test.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_evalRPN(t *testing.T) {
+	assert := assert.New(t)
+
+	assert.Equal(9, evalRPN([]string{
+		"2", "1", "+", "3", "*",
+	}))
+	assert.Equal(6, evalRPN([]string{
+		"4", "13", "5", "/", "+",
+	}))
+	assert.Equal(22, evalRPN([]string{
+		"10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+",
+	}))
+}
